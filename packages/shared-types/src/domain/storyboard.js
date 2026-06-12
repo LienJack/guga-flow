162ef28637ebefa777a6dfa305382ac5104a6fbe
LienJack <longyuhuhao@126.com>
2026-06-12 19:39:28 +0800

    chore: remove generated shared package artifacts

--- packages/shared-types/src/domain/storyboard.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=storyboard.js.map
--- packages/shared-types/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./domain/assets";
-export * from "./domain/canvas";
-export * from "./domain/generation";
-export * from "./domain/project";
-export * from "./domain/storyboard";
-//# sourceMappingURL=index.d.ts.map
